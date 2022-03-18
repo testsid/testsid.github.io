34e84f198d3ea7dc1638b70dacd934dfca360bef
Dina <sharipova0308@gmail.com>
2022-03-18 11:15:19 +0500

    add promo

--- js/script.js
+++ js/script.js
@@ -17,35 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    //process slider
-    const swiper = new Swiper('.swiper', {
-       
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      
-       
-        effect: "coverflow",
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: "auto",
-        coverflowEffect: {
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        },
-        loop: true,
-      });
+    
 
      //modal 
     
