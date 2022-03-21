c47e7579f3237e7efc352e717efaae6e75b19300
Dina <sharipova0308@gmail.com>
2022-03-21 16:11:22 +0500

    add media

--- js/script.js
+++ js/script.js
@@ -10,12 +10,12 @@ window.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('menu_active');
     });
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        });
-    });
+    // menuItem.forEach(item => {
+    //     item.addEventListener('click', () => {
+    //         hamburger.classList.toggle('hamburger_active');
+    //         menu.classList.toggle('menu_active');
+    //     });
+    // });
 
     //show submenu
 $('.menu__item').each(function(i) {
