8f0deaa95d8e96fad2c297d384f40f65dd891b23
Dina <sharipova0308@gmail.com>
2022-06-07 17:01:18 +0500

    add tehnology

--- js/script.js
+++ js/script.js
@@ -31,6 +31,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
      
 
+      //tabs
+      
+        $(function() {
+          
+          $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+            $(this)
+              .addClass('active').siblings().removeClass('active')
+              .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+          });
+          
+        });
       
 
 
